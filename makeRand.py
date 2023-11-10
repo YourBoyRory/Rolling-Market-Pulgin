5d4d371958f48c9d4581d9f967920e9a65dd8c39
YourBoyRory <yourboyrory@gmail.com>
2023-11-10 17:34:28 -0500

    script

--- makeRand.py
+++ makeRand.py
@@ -1,10 +1,35 @@
 import yaml
+import random
+import csv
+from pprint import pprint
+from urllib.request import urlretrieve
 
-with open('worthRange.yml', 'r') as file:
-    docs = yaml.safe_load_all(file)
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTFCLa96scKKGwVu77zSCAHW1JPs_Y8XswXykfGYYLUaJI7sV-pyar_BCkZlvKPiafMemV1jatSakg9/pub?output=csv"
+filename = './cache/worth.csv'
+try:
+    urlretrieve(url, filename)
+except:
+    print("Failed to download file")
+    pass
+
+newItems = {}
+newItems['worth'] = {} 
+
+worthRangeDictionary = {}
+
+with open('./cache/worth.csv') as f:
+   for line in csv.DictReader(f, fieldnames=('item', 'max', 'min')):
+        worthRangeDictionary[line['item']] = {}
+        worthRangeDictionary[line['item']]['max'] = float(line['max'])
+        worthRangeDictionary[line['item']]['min'] = float(line['min'])
     
-    for doc in docs:
-        for block in doc:
-            print(block+":")
-            print("  Max: " + str(doc[block]['max']))
-            print("  Min: " + str(doc[block]['min']))
+
+for item in worthRangeDictionary:
+    newItems['worth'][item] = round(random.uniform(worthRangeDictionary[item]['min'], worthRangeDictionary[item]['max']), 2)
+
+
+with open('worth.yml', 'w') as worthFile:
+    yaml.dump(newItems, worthFile)
+
+with open('worthRange.yml', 'w') as worthRangeFile:
+    yaml.dump(worthRangeDictionary, worthRangeFile)
